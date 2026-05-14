9f7cfb804855e375149a70edc9b78ca7999efed6
최혜민 <choihyemin@choehyemin-ui-MacBookAir.local>
2026-05-14 09:07:04 +0900

    260513

--- CodeTree/src/Main.java
+++ CodeTree/src/Main.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
+	static int N;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);		
+		N = sc.nextInt();
+		
+		int[] dp = new int[N + 1];
+		
+		dp[0] = 1;
+		if(N >= 1) dp[1] = 1;
+		
+		for(int i = 2; i <= N; i++) {
+			for(int j = 1; j <= i; j++) {
+				dp[i] += dp[j - 1] * dp[i - j];
+			}
+		}
+		
+		System.out.println(dp[N]);
+	}
 	
 }
