2c1c6e3fd9fdd268fb26886c93f9dec387ff4cb0
최혜민 <choihyemin@choehyemin-ui-MacBookAir.local>
2026-05-20 22:52:54 +0900

    260520

--- CodeTree/src/Main.java
+++ CodeTree/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -19,10 +20,43 @@ public class Main {
 		int minD = INF;
 		
 		
-		for(int i = 1; i <= 100; i++) {
+		for(int l = 1; l <= 100; l++) {
 			int[][] dp = new int[N][N];
 			
-			for(int i = 0; i < N; i)
+			if(grid[0][0] < l) continue;
+			
+			for(int i = 0; i < N; i++) {
+				Arrays.fill(dp[i], INF);
+			}
+			
+			dp[0][0] = grid[0][0];
+			
+			for(int i = 0; i < N; i++) {
+				for(int j = 0; j < N; j++) {
+					if(i == 0 && j == 0 || grid[i][j] < l) continue;
+					
+					int up = INF;
+					int left = INF;
+					
+					if(i - 1 > 0 && dp[i - 1][j] != INF) {
+						up = Math.max(grid[i][j], dp[i - 1][j]);
+					}
+					
+					if(j - 1 > 0 && dp[i][j - 1] != INF) {
+						left = Math.max(grid[i][j], dp[i][j - 1]);
+					}
+					
+					dp[i][j] = Math.min(up, left);
+					
+					if(dp[N - 1][N - 1] != INF) {
+						int diff = dp[N - 1][N - 1] - l;
+						
+						minD = Math.min(minD, diff);
+					}
+				}
+			}
 		}
+		
+		System.out.println(minD);
 	}
 }
