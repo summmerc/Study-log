27ec3d01f5faa248ca308373bad2921f1c5318a7
최혜민 <choihyemin@choehyemin-ui-MacBookAir.local>
2026-05-25 22:42:37 +0900

    260525

--- CodeTree/src/Main.java
+++ CodeTree/src/Main.java
@@ -1,67 +1,44 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.Scanner;
-
 
 public class Main {
 	static int N;
-	static class Point implements Comparable<Point> {
-		int x, v, index;
-		
-		public Point(int x, int v, int index) {
-			this.x = x;
-			this.v = v;
-			this.index = index;
-		}
-		
-		@Override
-		public int compareTo(Point p) {
-			if(this.x != p.x) {
-				return Integer.compare(this.x, p.x);
-			}
-			return Integer.compare(p.v, this.v);
-		}
-	}
+	static int M;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
 		N = sc.nextInt();
-		ArrayList<Point> points = new ArrayList<>();
+		M = sc.nextInt();
+		
+		int[] e = new int[N];
+		int[] t = new int[N];
+		
+		int max = 0;
+		for(int i = 0; i < N; i++) {
+			e[i] = sc.nextInt();
+			t[i] = sc.nextInt();
+			max += t[i];
+		}
+		
+		int[] dp = new int[max + 1];
 		
 		for(int i = 0; i < N; i++) {
-			int x1 = sc.nextInt();
-			int x2 = sc.nextInt();
+			int currE = e[i];
+			int currT = t[i];
 			
-			points.add(new Point(x1, 1, i));
-			points.add(new Point(x2, -1, i));
-		}
-		
-		Collections.sort(points);
-		
-		long length = 0;
-		HashSet<Integer> set = new HashSet<>();
-		
-		for(int i = 0; i < 2 * N; i++) {
-			int x = points.get(i).x;
-			int v = points.get(i).v;
-			int idx = points.get(i).index;
-			
-			
-			if(v == 1) {
-				set.add(idx);
-			} else {
-				set.remove(idx);
-			}
-			
-			if(i < 2 * N - 1) {
-				int next = points.get(i + 1).x;
-				if(next > x && set.size() > 0) {
-					length += (next - x);					
-				}
+			for(int time = max; time >= currT; time--) {
+				dp[time] = Math.max(dp[time], dp[time - currT] + currE);
 			}
 		}
 		
-		System.out.println(length);
+		int ans = -1;
+		for(int time = 0; time <= max; time++) {
+			if(dp[time] >= M) {
+				ans = time;
+				break;
+			}
+		}
+		
+		System.out.println(ans);
+		
 	}
 }
