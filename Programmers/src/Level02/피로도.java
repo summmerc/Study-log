0af67b7abed062aeb202d40d0691f4566931482d
최혜민 <choihyemin@choehyemin-ui-MacBookAir.local>
2026-07-02 16:14:20 +0900

    260702

---
+++ Programmers/src/Level02/피로도.java
@@ -0,0 +1,24 @@
+package Level02;
+
+class 피로도 {
+	static boolean[] visited;
+	static int max;
+    public int solution(int k, int[][] dungeons) {
+        
+    	visited = new boolean[dungeons.length];
+    	dfs(k, 0, dungeons);
+        return max;
+    }
+    
+    static void dfs(int k, int cnt, int[][] dungeons) {
+    	max = Math.max(max, cnt);
+    	
+    	for(int i = 0; i < dungeons.length; i++) {
+    		if(!visited[i] && dungeons[i][0] <= k) {
+    			visited[i] = true;
+    			dfs(k - dungeons[i][1], cnt + 1, dungeons);
+    			visited[i] = false;
+    		}
+    	}
+    }
+}
---
+++ Programmers/src/Level03/네트워크.java
@@ -0,0 +1,58 @@
+package Level03;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+class 네트워크 {
+	static boolean[] visited;
+	static int cnt;
+	static List<Integer>[] network;
+	
+    public int solution(int n, int[][] computers) {
+    	network = new ArrayList[n];
+    	
+    	for(int i = 0; i < n; i++) {
+    		network[i] = new ArrayList<>();
+    	}
+    	
+    	for(int i = 0; i < n; i++) {
+    		for(int j = 0; j < n; j++) {
+    			if(i == j) continue;
+    			if(computers[i][j] == 1) {
+    				network[i].add(j);
+    			}
+    		}
+    	}
+    	
+    	cnt = 0;
+    	visited = new boolean[n];
+    	for(int i = 0; i < n; i++) {
+    		if(!visited[i]) {
+    			bfs(i, n);
+    		}
+    	}
+    	
+        return cnt;
+    }
+    
+    static void bfs(int start, int n) {
+    	visited[start] = true;
+    	Queue<Integer> q = new ArrayDeque<>();
+    	
+    	q.add(start);
+    	
+    	while(!q.isEmpty()) {
+    		int cur = q.poll();
+    		for(int i = 0; i < network[cur].size(); i++) {
+    			if(!visited[network[cur].get(i)]) {
+    				q.add(network[cur].get(i));
+    				visited[network[cur].get(i)] = true;
+    			}
+    		}
+    	}
+    	
+    	cnt++;
+    }
+}
