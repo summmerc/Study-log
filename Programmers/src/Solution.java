3dcddac39fd8f4056f09819971d19961808a41b3
최혜민 <choihyemin@choehyemin-ui-MacBookAir.local>
2026-07-02 16:14:37 +0900

    260702

--- Programmers/src/Solution.java
+++ Programmers/src/Solution.java
@@ -1,6 +1,56 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
 class Solution {
-    public int solution(int k, int[][] dungeons) {
-        int answer = -1;
-        return answer;
+	static boolean[] visited;
+	static int cnt;
+	static List<Integer>[] network;
+	
+    public int solution(int n, int[][] computers) {
+    	network = new ArrayList[n];
+    	
+    	for(int i = 0; i < n; i++) {
+    		network[i] = new ArrayList<>();
+    	}
+    	
+    	for(int i = 0; i < n; i++) {
+    		for(int j = 0; j < n; j++) {
+    			if(i == j) continue;
+    			if(computers[i][j] == 1) {
+    				network[i].add(j);
+    			}
+    		}
+    	}
+    	
+    	cnt = 0;
+    	visited = new boolean[n];
+    	for(int i = 0; i < n; i++) {
+    		if(!visited[i]) {
+    			bfs(i, n);
+    		}
+    	}
+    	
+        return cnt;
+    }
+    
+    static void bfs(int start, int n) {
+    	visited[start] = true;
+    	Queue<Integer> q = new ArrayDeque<>();
+    	
+    	q.add(start);
+    	
+    	while(!q.isEmpty()) {
+    		int cur = q.poll();
+    		for(int i = 0; i < network[cur].size(); i++) {
+    			if(!visited[network[cur].get(i)]) {
+    				q.add(network[cur].get(i));
+    				visited[network[cur].get(i)] = true;
+    			}
+    		}
+    	}
+    	
+    	cnt++;
     }
 }
