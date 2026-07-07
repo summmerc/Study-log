7fd10bf2421622a13f9d73fb6f4608c8529e911b
최혜민 <choihyemin@choehyemin-ui-MacBookAir.local>
2026-07-07 21:09:40 +0900

    260707

---
+++ Programmers/src/Level02/과제진행하기.java
@@ -0,0 +1,67 @@
+package Level02;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Stack;
+
+class 과제진행하 {
+	static PriorityQueue<String[]> pq;
+	static Stack<String[]> st;
+    public String[] solution(String[][] plans) {
+    	Arrays.sort(plans, (o1, o2) -> o1[1].compareTo(o2[1]));
+    	
+    	st = new Stack<>();
+    	String[] answer = new String[plans.length];
+    	
+    	
+    	int now = 0; //시간
+    	int done = 0; //끝난거 answer넣을거
+    	int idx = 0; //지금 진행중인
+    	
+    	while(idx < plans.length - 1) {
+    		String[] curr = plans[idx];
+    		
+    		String[] time = curr[1].split(":");
+    		int s = Integer.parseInt(time[0]) * 60 + Integer.parseInt(time[1]);
+    		int du = Integer.parseInt(curr[2]);
+    		
+    		String[] next = plans[idx + 1];
+    		String[] time2 = next[1].split(":");
+    		int s2 = Integer.parseInt(time2[0]) * 60 + Integer.parseInt(time2[1]);
+    		int du2 = Integer.parseInt(next[2]);
+    		
+    		now = s2 - s - du;
+    		if(s + du <= s2) { //만약에 ~ 시간이 남을 때
+    			answer[done++] = curr[0];
+//    			int diff = s2 - now;
+    			
+    			while(!st.isEmpty()) {
+    				String[] spop = st.pop();
+    				now -= Integer.parseInt(spop[2]);
+    				
+    				if(now < 0) {
+    					spop[2] = Math.abs(now) + "";
+    					st.add(spop);
+    					break;
+    				}
+    				
+    				answer[done++] = spop[0];
+    			}
+    			
+    		}
+    		else { 
+    			curr[2] = Math.abs(now) + "";
+    			st.add(curr);
+    		}
+    		idx++;
+    		
+    	}
+    	
+    	st.push(plans[idx]);
+    	while(!st.isEmpty()) {
+    		answer[done++] = st.pop()[0]; 
+    	}
+        
+        return answer;
+    }
+}
--- Programmers/src/Solution.java
+++ Programmers/src/Solution.java
@@ -1,65 +1,50 @@
 import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.Stack;
+import java.util.HashSet;
 
 class Solution {
-	static PriorityQueue<String[]> pq;
-	static Stack<String[]> st;
-    public String[] solution(String[][] plans) {
-    	Arrays.sort(plans, (o1, o2) -> o1[1].compareTo(o2[1]));
+	static boolean[] visited;
+	static HashSet<String> set;
+	static int[] picked;
+
+	public int solution(String[] user_id, String[] banned_id) {
+		visited = new boolean[user_id.length];
+		set = new HashSet<>();
+		picked = new int[banned_id.length];
+
+		dfs(0, user_id, banned_id);
+
+		return set.size();
+	}
+
+	static void dfs(int idx, String[] user_id, String[] banned_id) {
     	
-    	st = new Stack<>();
-    	String[] answer = new String[plans.length];
-    	
-    	
-    	int now = 0; //시간
-    	int done = 0; //끝난거 answer넣을거
-    	int idx = 0; //지금 진행중인
-    	
-    	while(idx < plans.length - 1) {
-    		String[] curr = plans[idx];
-    		
-    		String[] time = curr[1].split(":");
-    		int s = Integer.parseInt(time[0]) * 60 + Integer.parseInt(time[1]);
-    		int du = Integer.parseInt(curr[2]);
-    		
-    		String[] next = plans[idx + 1];
-    		String[] time2 = next[1].split(":");
-    		int s2 = Integer.parseInt(time2[0]) * 60 + Integer.parseInt(time2[1]);
-    		int du2 = Integer.parseInt(next[2]);
-    		
-    		now = s2 - s - du;
-    		if(s + du <= s2) { //만약에 ~ 시간이 남을 때
-    			answer[done++] = curr[0];
-//    			int diff = s2 - now;
-    			
-    			while(!st.isEmpty()) {
-    				String[] spop = st.pop();
-    				now -= Integer.parseInt(spop[2]);
-    				
-    				if(now < 0) {
-    					spop[2] = Math.abs(now) + "";
-    					st.add(spop);
-    					break;
-    				}
-    				
-    				answer[done++] = spop[0];
-    			}
-    			
-    		}
-    		else { 
-    			curr[2] = Math.abs(now) + "";
-    			st.add(curr);
-    		}
-    		idx++;
-    		
+    	if(idx == banned_id.length) {
+    		int[] copy = picked.clone();
+    		Arrays.sort(copy);
+    		set.add(Arrays.toString(copy));
+    		return;
     	}
     	
-    	st.push(plans[idx]);
-    	while(!st.isEmpty()) {
-    		answer[done++] = st.pop()[0]; 
+    	for(int i = 0; i < user_id.length; i++) {
+    		if(visited[i]) continue;
+    		if(isMatch(user_id[i], banned_id[idx])) {
+    			visited[i] = true;
+    			picked[idx] = i;
+    			
+    			dfs(idx + 1, user_id, banned_id);
+    			
+    			visited[i] = false;
+    		}
     	}
-        
-        return answer;
     }
+
+	static boolean isMatch(String user, String banned) {
+		if(user.length() != banned.length()) return false;
+		for(int i = 0; i < banned.length(); i++) {
+			if(banned.charAt(i) == '*') continue;
+			if(user.charAt(i) != banned.charAt(i)) return false;
+		}
+		return true;
+	}
+
 }
